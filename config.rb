fe0c87ad934beaf0dc415976cd7b786c00b70ba1
unknown <jana@jbee.io>
2014-05-14 01:33:38 -0700

    widget and changes and everything else

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'bootstrap-sass'
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+
+output_style = :compressed #:expanded :nested :compact :compressed
+
+
+# relative_assets = true
+# line_comments = false
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
